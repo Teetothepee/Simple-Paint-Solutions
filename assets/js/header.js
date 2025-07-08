36014aac570392b99af93a9d675fa1023dfe7758
Teetothepee <97924659+Teetothepee@users.noreply.github.com>
2025-07-08 16:51:40 -0700

    Update header.js

--- assets/js/header.js
+++ assets/js/header.js
@@ -3,7 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const navbarTarget = document.getElementById('navbar');
   if (navbarTarget) {
     const basePath = window.location.origin + window.location.pathname.split('/').slice(0, -1).join('/');
-    fetch('/header.html')
+    fetch('https://teetothepee.github.io/Simple-Paint-Solutions/header.html')
+
 
       .then(res => res.text())
       .then(html => {
